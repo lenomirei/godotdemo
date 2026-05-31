f55a1a9514ad79f558783f7a7dcae5b5b2eb0a60
lenomirei <lenomirei@163.com>
2026-05-31 20:03:49 +0800

    [feat] Fix attack2 animation total time. Add attack buffer input

--- player.gd
+++ player.gd
@@ -9,22 +9,24 @@ var weapon_state: WeaponState = WeaponState.HOLSTERED
 var facing_left = false
 var was_on_floor = false
 var state: State = State.IDLE
-var state_lock = false
+var action_lock = false
 var attack_combo = 0
+var next_state = State.INVALID
 
-enum State { IDLE, WALK, ATTACKING, JUMPING, FALL, LANDING }
+enum State { IDLE, WALK, ATTACKING, JUMPING, FALL, LANDING, INVALID }
 
 func try_attack() -> void:
-	if state_lock:
+	if action_lock:
+		next_state = State.ATTACKING
 		return
 	if weapon_state == WeaponState.HOLSTERED:
 		weapon_state = WeaponState.DRAWN
 		
 	state = State.ATTACKING
-	state_lock = true
+	action_lock = true
 	
 	if state == State.ATTACKING:
-		attack_combo += 1
+		pass
 	else:
 		state = State.ATTACKING
 	
@@ -47,13 +49,13 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and not state_lock:
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and not action_lock:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction and !state_lock:
+	if direction and !action_lock:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
@@ -72,12 +74,12 @@ func _physics_process(delta: float) -> void:
 
 func on_landing() -> void:
 	state = State.LANDING
-	state_lock = true
+	action_lock = true
 	velocity = Vector2(0, 0)
 	$AnimationPlayer.play("landing")
 
 func update_state() -> void:
-	if state_lock:
+	if action_lock:
 		return
 	if velocity.y != 0:
 		if velocity.y > 600:
@@ -113,21 +115,33 @@ func get_animation_name() -> String:
 		State.LANDING:
 			animation_name = "landing" + suffix
 		State.ATTACKING:
-			animation_name = "attack" + String.num_int64(attack_combo)
+			animation_name = "attack" + String.num_int64(attack_combo + 1)
 	return animation_name
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name.begins_with("landing"):
-		state_lock = false
-	if anim_name.begins_with("attack"):
-		state_lock = false
+		action_lock = false
+	if anim_name.begins_with("attack") and next_state != State.ATTACKING:
+		action_lock = false
 		$AttackComboTimer.start()
+	elif anim_name.begins_with("attack") and next_state == State.ATTACKING:
+		state = next_state
+		next_state = State.INVALID
+		action_lock = true
+		attack_combo += 1
 	
 	if anim_name == "attack4":
 		attack_combo = 0
-		print("combo is over")
+		print("attack combo is finished")
+		
+	#if next_state != State.INVALID:
+		#state = next_state
+		#next_state = State.INVALID
+		#if state == State.ATTACKING:
+			#print("attack continue")
+			#action_lock = true
+			#attack_combo += 1
 
 
 func _on_attack_combo_timer_timeout() -> void:
-	print("combo timeout")
 	attack_combo = 0
