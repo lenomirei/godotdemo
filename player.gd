b36d15a9d87b0471aa20d68c86f4d664f1973378
lenomirei <lenomirei@163.com>
2026-06-02 18:53:07 +0800

    [feat] Add ladder.

--- player.gd
+++ player.gd
@@ -13,7 +13,17 @@ var action_lock = false
 var attack_combo = 0
 var next_state = State.INVALID
 
-enum State { IDLE, WALK, ATTACKING, JUMPING, FALL, LANDING, ROLLING, INVALID }
+enum State { IDLE, WALK, ATTACKING, JUMPING, FALL, LANDING, ROLLING, LADDERING, INVALID }
+
+func is_on_ladder() -> bool:
+	var tilemap: TileMapLayer = get_parent().get_node("TileMapLayer")
+	var cell = tilemap.local_to_map(
+		tilemap.to_local(global_position)
+	)
+	var tile_data = tilemap.get_cell_tile_data(cell)
+	if tile_data == null:
+		return false
+	return tile_data.get_custom_data("ladder")
 
 func try_attack() -> void:
 	if action_lock:
@@ -39,6 +49,11 @@ func try_rolling() -> void:
 	state = State.ROLLING
 	action_lock = true
 
+func try_climbing() -> void:
+	if !is_on_ladder():
+		return;
+	state = State.LADDERING
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("drawn_weapon"):
 		if weapon_state == WeaponState.HOLSTERED:
@@ -51,6 +66,9 @@ func _unhandled_input(event: InputEvent) -> void:
 	
 	if event.is_action_pressed("rolling"):
 		try_rolling()
+		
+	if event.is_action_pressed("climb"):
+		try_climbing()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -60,6 +78,14 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and not action_lock:
 		velocity.y = JUMP_VELOCITY
+	
+	var ladder_direction := Input.get_axis("ui_up", "ui_down")
+	if is_on_ladder() and state == State.LADDERING:
+		if ladder_direction == 0:
+			$AnimationPlayer.speed_scale = 0
+		else:
+			$AnimationPlayer.speed_scale = 1
+		velocity.y = ladder_direction * SPEED
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -81,6 +107,7 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	if !was_on_floor and is_on_floor():
 		on_landing()
+		
 	was_on_floor = is_on_floor()
 	update_state()
 	update_animation()
@@ -93,6 +120,12 @@ func on_landing() -> void:
 
 func update_state() -> void:
 	if action_lock:
+		return
+	if !is_on_ladder() and state == State.LADDERING:
+		state = State.IDLE
+		return
+	elif is_on_ladder() and state == State.LADDERING:
+		state = State.LADDERING
 		return
 	if velocity.y != 0:
 		if velocity.y > 600:
@@ -124,13 +157,15 @@ func get_animation_name() -> String:
 		State.JUMPING:
 			animation_name = "jump"
 		State.FALL:
-			animation_name = "fall" + suffix
+			animation_name = "fall"
 		State.LANDING:
 			animation_name = "landing" + suffix
 		State.ATTACKING:
 			animation_name = "attack" + String.num_int64(attack_combo)
 		State.ROLLING:
 			animation_name = "rolling"
+		State.LADDERING:
+			animation_name = "laddering"
 	return animation_name
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
