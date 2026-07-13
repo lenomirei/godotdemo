23bdd91c1c32dcf5a52e558af7839796ad0c621d
lenomirei <lenomirei@163.com>
2026-07-13 22:34:26 +0800

    [bugfix] Fix the monster hit animation. Fix player attack area.

--- monster/states/motion/on_ground/hit.gd
+++ monster/states/motion/on_ground/hit.gd
@@ -3,4 +3,8 @@ extends "res://monster/states/motion/on_ground/on_ground.gd"
 
 func enter() -> void:
 	print("enter hit")
-	owner.get_node(^"AnimationPlayer").play("hit")
+	var animation_player: AnimationPlayer = owner.get_node(^"AnimationPlayer")
+	if animation_player.current_animation == "hit":
+		animation_player.seek(0, true, false)
+	else:
+		animation_player.play("hit")
